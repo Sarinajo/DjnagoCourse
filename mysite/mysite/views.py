d7b394332faad3be92235efd003d4e663465c039
Sarina Joshi <joshisarina19@gmail.com>
2025-12-11 13:13:32 +0545

    commit

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,59 @@
 #i created this
+'''{% comment %}
+when we do install django
+
+and do django-admin, it gives subcommands that we can use.
+one of them is startproject. we can use startproject to start our project
+when we do django-admin startproject projectname
+a project is created of the project name
+projectname
+	projectname
+		manage.py
+
+manage.py is a command line utility. it helps to interact with django project
+
+when we do
+	 python3 manage.py runserver
+the server runs
+
+
+django view
+ it is a python function that recieves web request and returns web reposne.
+every view must return HttpResponse object
+
+the it should be connected to URLs
+
+in urls.py, add the path that will lead to view
+path(route, view, kwargs=none, name=none)
+
+foreg in views.py we have
+def index(request):
+	return Httpresponse(‘hello)
+
+in urls.py
+
+in urlpatterns = [
+path(‘’, views.index, name = ‘index’)
+
+
+
+]'''
+
+
 from django.http import HttpResponse
+import os
 
 
 def index(request):
     return HttpResponse('hello sj')
+
+def about(request):
+	return HttpResponse('hello this is about')
+
+def fileo(request):
+	file_dir = os.path.dirname(__file__)
+	file_path = os.path.join(file_dir,'one.txt')
+	with open(file_path,'r') as f:
+		content = f.read()
+		return HttpResponse(content)
+
